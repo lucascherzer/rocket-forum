6c1fb501e072dd437ea1a8db227952e174d81292
Luca Scherzer <9883683-lucascherzer@users.noreply.gitlab.com>
2025-06-05 12:15:28 +0200

    style: clean up unused stuff

--- src/redis.rs
+++ src/redis.rs
@@ -1,12 +1,7 @@
-use dotenv::dotenv;
 use r2d2_redis::{
     RedisConnectionManager,
     r2d2::{self, Pool},
 };
-use rocket::http::Status;
-use rocket::request::{self, FromRequest};
-use rocket::{Request, State, async_trait};
-use std::ops::{Deref, DerefMut};
 
 pub fn get_redis(redis_url: String) -> Pool<RedisConnectionManager> {
     let manager =
@@ -15,19 +10,3 @@ pub fn get_redis(redis_url: String) -> Pool<RedisConnectionManager> {
         .build(manager)
         .expect("failed to build redis pool")
 }
-
-// #[async_trait]
-// impl<'r> FromRequest<'r> for Conn {
-//     type Error = ();
-
-//     async fn from_request(request: &'r rocket::Request<'_>) -> request::Outcome<Self, Self::Error> {
-//         let pool = match request.guard::<State<Pool>>().await {
-//             request::Outcome::Success(pool) => pool,
-//             _ => return request::Outcome::Forward(()),
-//         };
-//         match pool.get() {
-//             Ok(database) => Outcome::Success(Conn(database)),
-//             Err(_) => Outcome::Failure((Status::ServiceUnavailable, ())),
-//         }
-//     }
-// }
